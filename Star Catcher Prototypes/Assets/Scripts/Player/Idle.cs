81b8db414756a25a8c048b888b03cff31c003ff9
Kirsti Motter <hopskipbye@gmail.com>
2016-10-18 13:02:25 -0600

    Bunny & Wolf
    Added idle blink and set up enemy layering.

---
+++ Star Catcher Prototypes/Assets/Scripts/Player/Idle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Idle : MonoBehaviour {
+
+	const int BLINK_FREQUENCY = 5;
+
+	private Animator anim;
+	private int rand;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Blink()
+	{
+		rand = Random.Range (0, BLINK_FREQUENCY);
+		if (rand == 0) {
+			anim.Play ("BunnyIdleBlink");
+		}
+	}
+}
