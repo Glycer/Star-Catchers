915f87145aab34ec03006055168ce7674142b1ab
Glycer <hopskipbye@gmail.com>
2016-10-28 13:19:21 -0600

    Update WolfAI.cs

--- Star Catcher Prototypes/Assets/Scripts/Enemy/WolfAI.cs
+++ Star Catcher Prototypes/Assets/Scripts/Enemy/WolfAI.cs
@@ -36,3 +36,24 @@ public class WolfAI : MonoBehaviour {
 		}
 	}
 }
+
+//Make this a separate script
+public class WolfAttack : MonoBehaviour {
+	
+	private Animator anim;
+	
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	void OnTriggerEnter()
+	{
+		anim.Play("Attack");
+	}
+	
+	void Attack(GameObject target)
+	{
+		target.GetComponent<Animator>().Play("TakeHit");
+	}
+}
