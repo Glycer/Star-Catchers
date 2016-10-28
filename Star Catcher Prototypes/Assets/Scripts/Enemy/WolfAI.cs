f42361347025c9b71d0ed346637c6d36259504c1
Glycer <hopskipbye@gmail.com>
2016-10-28 13:10:22 -0600

    Update WolfAI.cs

--- Star Catcher Prototypes/Assets/Scripts/Enemy/WolfAI.cs
+++ Star Catcher Prototypes/Assets/Scripts/Enemy/WolfAI.cs
@@ -5,20 +5,34 @@ public class WolfAI : MonoBehaviour {
 
 	public float runSpeed;
 
+	private bool canRun;
 	private Rigidbody rigidBody;
-	private Vector3 jumpForce = new Vector3(0, 50, 0);
+	private Vector3 jumpForce = new Vector3(0, 300, 0);
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody> ();
-	}
-
-	void Update () {
-		transform.Translate (runSpeed * Time.deltaTime, 0, 0);
+		canRun = true;
+		
+		StartCoroutine(Run());
 	}
 
 	public void Jump()
 	{
 		rigidBody.AddForce (jumpForce);
+	}
+	
+	public void Stop()
+	{
+		canRun = false;
+	}
+	
+	IEnumerator Run()
+	{
+		while (canRun)
+		{
+			transform.Translate (runSpeed * Time.deltaTime, 0, 0);
+			yield return;
+		}
 	}
 }
