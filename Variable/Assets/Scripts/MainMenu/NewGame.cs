986a73fe8b1ab120e99d7cacd01c72cce8cd14ea
K.R.Motter <hopskipbye@gmail.com>
2016-11-16 22:24:54 -0700

    Started Navigation Controls

---
+++ Variable/Assets/Scripts/MainMenu/NewGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NewGame : MonoBehaviour {
+
+	/// <summary>
+	/// Starts the game.
+	/// </summary>
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("Main");
+	}
+}
---
+++ Variable/Assets/Scripts/Navigation/ToggleScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToggleScreen : MonoBehaviour {
+
+	public List<GameObject> screens;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < transform.childCount; i++) {
+			screens.Add (transform.GetChild(i));
+		}
+		
+	}
+
+	public void Toggle(GameObject activeScreen)
+	{
+		foreach (GameObject child in screens) {
+			if (child == activeScreen) {
+				activeScreen.SetActive (true);
+			} else {
+				activeScreen.SetActive (false);
+			}
+		}
+	}
+}
