c2d17530d9dcfda9fee16075223d17582b4fdc8f
K.R.Motter <hopskipbye@gmail.com>
2016-11-21 17:51:50 -0700

    Variable
    Added Generic Navigation Functions

---
+++ Variable/Assets/Scripts/Navigation/StaticLocations.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticLocations {
+
+	public enum Locations {Park, Mall, Graveyard};
+
+	public static Locations location = Locations.Park;
+}
--- Variable/Assets/Scripts/Navigation/ToggleScreen.cs
+++ Variable/Assets/Scripts/Navigation/ToggleScreen.cs
@@ -2,25 +2,47 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// <summary>
+/// Toggle screens. This is designed to be placed on an object, such as a canvas,
+/// 	with its children organized into self-contained screens.
+/// </summary>
 public class ToggleScreen : MonoBehaviour {
 
+	//For toggling between scenes
+	public string sceneName;
+
+	//For toggling between views in a single scene
 	public List<GameObject> screens;
 
 	// Use this for initialization
 	void Start () {
+		//Fill screens List with this object's children
 		for (int i = 0; i < transform.childCount; i++) {
-			screens.Add (transform.GetChild(i));
+			screens.Add (transform.GetChild(i).gameObject);
 		}
 		
 	}
 
-	public void Toggle(GameObject activeScreen)
+	/// <summary>
+	/// Toggle between scenes.
+	/// </summary>
+	/// <param name="sceneName">String, refers to the name of the scene to be loaded.</param>
+	public void Toggle (string sceneName)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene (sceneName);
+	}
+
+	/// <summary>
+	/// Toggle between views in a single scene.
+	/// </summary>
+	/// <param name="activeScreenIndex">Int, refers to the index of an object in screens.</param>
+	public void Toggle(int activeScreenIndex)
 	{
 		foreach (GameObject child in screens) {
-			if (child == activeScreen) {
-				activeScreen.SetActive (true);
+			if (screens[activeScreenIndex] == child) {
+				child.SetActive (true);
 			} else {
-				activeScreen.SetActive (false);
+				child.SetActive (false);
 			}
 		}
 	}
