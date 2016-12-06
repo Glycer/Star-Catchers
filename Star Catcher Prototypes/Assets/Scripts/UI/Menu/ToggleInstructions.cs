24afc4a746a4dc5a84636dd7e399fbda95f2e9da
Kirsti Motter <hopskipbye@gmail.com>
2016-12-06 13:20:49 -0700

    Star Catchers
    Scripts cleaned, instructions cleaned.

---
+++ Star Catcher Prototypes/Assets/Scripts/UI/Menu/ToggleInstructions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleInstructions : MonoBehaviour {
+
+	public GameObject instructions;
+
+	// Use this for initialization
+	void Start () {
+		instructions.SetActive (false);
+	}
+
+	public void Pop()
+	{
+		if (instructions.activeInHierarchy) {
+			instructions.SetActive (false);
+		} else {
+			instructions.SetActive (true);
+		}
+	}
+}
