eb1bfc3e99fe0f4e4404ef0b6e35a9a4a9b7b309
Unknown <hopskipbye@gmail.com>
2019-08-08 15:50:30 -0600

    File Cleanup
    Deleted 'Variable' sideproject

--- Variable/Assets/Scripts/Battle/Actors/IAttack.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IAttack {
-
-	BattleSprite target { get; set; }
-
-	void DealDamage(BattleSprite tar);
-}
--- Variable/Assets/Scripts/Battle/Actors/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IDamageable {
-
-	int health { get; set; }
-
-	void TakeDamage(int dmg);
-}
